cb3697d034a3500ecc4c3ad77ba05f73f595264b
Tsukasa <tsukasa.kzk@gmail.com>
2018-02-10 15:53:11 +0800

    number led and show core function

--- number.cpp
+++ number.cpp
@@ -45,17 +45,35 @@ class number
         0, 0, 0, 0, 0, 0, 0, 0};
     // number
     // DP, middle, left_top, left_bot, bot, right_bot, right_top, top
+    int num_led_0[8] = {1, 1, 0, 0, 0, 0, 0, 0};
     int num_led_1[8] = {1, 1, 1, 1, 1, 0, 0, 1};
-    int num_led_2[8] = {1, 0, 0, 0, 0, 0, 1, 0}; //
+    int num_led_2[8] = {1, 0, 1, 0, 0, 1, 0, 0};
+    int num_led_3[8] = {1, 0, 1, 1, 0, 0, 0, 0};
+    int num_led_4[8] = {1, 0, 0, 1, 1, 0, 0, 1};
+    int num_led_5[8] = {1, 0, 0, 1, 0, 0, 1, 0};
     int num_led_6[8] = {1, 0, 0, 0, 0, 0, 1, 0};
-    int num_lef_8[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+    int num_led_7[8] = {1, 1, 1, 1, 1, 0, 0, 0};
+    int num_led_8[8] = {1, 0, 0, 0, 0, 0, 0, 0};
+    int num_led_9[8] = {1, 0, 0, 1, 0, 0, 0, 0};
+    // number with point
+    int num_led_0p[8] = {0, 1, 0, 0, 0, 0, 0, 0};
+    int num_led_1p[8] = {0, 1, 1, 1, 1, 0, 0, 1};
+    int num_led_2p[8] = {0, 0, 1, 0, 0, 1, 0, 0};
+    int num_led_3p[8] = {0, 0, 1, 1, 0, 0, 0, 0};
+    int num_led_4p[8] = {0, 0, 0, 1, 1, 0, 0, 1};
+    int num_led_5p[8] = {0, 0, 0, 1, 0, 0, 1, 0};
     int num_led_6p[8] = {0, 0, 0, 0, 0, 0, 1, 0};
-    int num_lef_8p[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int num_led_7p[8] = {0, 1, 1, 1, 1, 0, 0, 0};
+    int num_led_8p[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+    int num_led_9p[8] = {0, 0, 0, 1, 0, 0, 0, 0};
     // position
     int pos_led_5[8] = {0, 0, 0, 0, 1, 0, 0, 0};
     int pos_led_6[8] = {0, 0, 0, 0, 0, 1, 0, 0};
     int pos_led_7[8] = {0, 0, 0, 0, 0, 0, 1, 0};
     int pos_led_8[8] = {0, 0, 0, 0, 0, 0, 0, 1};
+
+    int i = 0;
+    void show_core(ledcode *);
 
   public:
     number(int r_sdi, int r_rclk, int r_srclk);
@@ -112,50 +130,41 @@ void number::pulse(int pin)
  */
 void number::loop_show()
 {
-    // DP, middle, left_top, left_bot, bot, right_bot, right_top, top
-    // int led[16] = {
-    //     0, 0, 0, 0, 0, 0, 0, 0,
-    //     0, 0, 0, 0, 1, 0, 0, 1};
-    // int led2[16] = {
-    //     1, 0, 0, 0, 1, 1, 1, 0,
-    //     0, 0, 0, 0, 0, 0, 0, 1};
-    ledcode lc(
-        this->num_led_6p,
-        this->pos_led_8);
-    int *led = lc.toarr();
-    int i;
+    ledcode *lc1 = new ledcode(this->num_led_3p, this->pos_led_5);
+    ledcode *lc2 = new ledcode(this->num_led_1, this->pos_led_6);
+    ledcode *lc3 = new ledcode(this->num_led_4, this->pos_led_7);
+    ledcode *lc4 = new ledcode(this->num_led_1, this->pos_led_8);
     do
     {
-        this->gpio_init();
-        for (i = 0; i < 16; i++)
-        {
-            ::digitalWrite(this->sdi, this->led_rst[i]);
-            this->pulse(this->srclk);
-        }
-        this->pulse(this->rclk);
+        this->show_core(lc1);
+        this->show_core(lc2);
+        this->show_core(lc3);
+        this->show_core(lc4);
+    } while (1);
+}
 
-        this->gpio_init();
-        for (i = 0; i < 16; i++)
-        {
-            ::digitalWrite(this->sdi, led[i]);
-            this->pulse(this->srclk);
-        }
-        this->pulse(this->rclk);
+/**
+ * 数码管显示核心函数
+ */
+void number::show_core(ledcode *lc)
+{
+    int *led = lc->toarr();
+    this->gpio_init();
+    for (this->i = 0; this->i < 16; this->i++)
+    {
+        ::digitalWrite(this->sdi, this->led_rst[this->i]);
+        this->pulse(this->srclk);
+    }
+    this->pulse(this->rclk);
 
-        // this->gpio_init();
-        // for (i = 0; i < 16; i++)
-        // {
-        //     ::digitalWrite(this->sdi, led2[i]);
-        //     this->pulse(this->srclk);
-        // }
-        // this->pulse(this->rclk);
+    this->gpio_init();
+    for (this->i = 0; this->i < 16; this->i++)
+    {
+        ::digitalWrite(this->sdi, led[this->i]);
+        this->pulse(this->srclk);
+    }
+    this->pulse(this->rclk);
 
-        // this->gpio_init();
-        // for (i = 0; i < 16; i++)
-        // {
-        //     ::digitalWrite(this->sdi, led_rst[i]);
-        //     this->pulse(this->srclk);
-        // }
-        // this->pulse(this->rclk);
-    } while (0);
+    free(led);
+    led = nullptr;
 }
